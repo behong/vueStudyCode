38c75643f68583dcae1b719836af3275d8b43a81
nayha <sijm@naver.com>
2018-11-07 20:52:06 +0900

    component 실습 v-bind

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+Vue.component(`friend-component`,{
+props:['friend'],
+filters :{
+    fullName(value){
+        return `${value.last} ${value.first}`
+    },
+    ageInOneYear(age){
+        return age +1;
+    }
+},
+methods :{
+    decremenAge(friend){
+        friend.age = friend.age + 1;
+    },        
+    incremenAge(friend){
+        friend.age = friend.age + 1;
+    },
+},
+template : 
+`
+    <div>
+        <h4>{{friend | fullName}}</h4>
+        <h5>age : {{friend.age}}</h5>
+        <button v-on:click="incremenAge(friend)"> + </button>
+        <button v-on:click="decremenAge(friend)"> - </button>
+        <input v-model="friend.first" />
+        <input v-model="friend.last" />
+    </div>
+`
+});
+
 const app = new Vue({
     el :"#app",
     data :{
@@ -32,14 +63,8 @@ const app = new Vue({
     },
     template : `
     <div>
-    <h2 v-for="friend in friends">
-        <h4>{{friend | fullName}}</h4>
-        <h5>age : {{friend.age}}</h5>
-        <button v-on:click="incremenAge(friend)"> + </button>
-        <button v-on:click="decremenAge(friend)"> - </button>
-        <input v-model="friend.first" />
-        <input v-model="friend.last" />
-    </h2>
+    <!--friend component 에서 가져온다 -->
+    <friend-component v-for="item in friends" v-bind:friend="item" />
     </div>
     `
 })
