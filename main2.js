433d4defcb88048f5b6a296d3e439c70a9c5fd78
nayha <sijm@naver.com>
2018-11-09 20:38:42 +0900

    리스트 데이터 수정 (언테키 수정)

--- main2.js
+++ main2.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el : "#app",
     data :{
+        editFriend : null,
         friends : [],
     },
     methods:{
@@ -13,6 +14,20 @@ const app = new Vue({
                 this.friends.splice(i,1);
             })
         }, 
+        updateFriend(item){
+            fetch("http://rest.learncode.academy/api/vue-5/friends/" + item.id, {
+                body : JSON.stringify(item),
+                method : "PUT",
+                headers :{
+                    "Content-Type" : "application/json"
+                },
+
+            })
+            .then(() =>{
+                //console.log("update")
+                this.editFriend =null;
+            })
+        },         
     },       
     mounted(){
         fetch("http://rest.learncode.academy/api/vue-5/friends")
@@ -26,7 +41,14 @@ const app = new Vue({
     `
     <div>
        <li v-for="item, i in friends">
-       <button v-on:click="delFriend(item.id,i)">x</button>{{item.name}}
+       <div v-if="editFriend === item.id">
+        <input v-on:keyup.13 ="updateFriend(item)" v-model="item.name" />
+        <button v-on:click="updateFriend(item)">save</button>
+       </div>
+       <div v-else>
+        <button v-on:click="editFriend = item.id">edit</button>
+        <button v-on:click="delFriend(item.id,i)">x</button>{{item.name}}
+       </div>
        </li>
     </div>
     `
